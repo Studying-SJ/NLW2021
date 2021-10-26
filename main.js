9b55a8af509ae2b5b3ad901d2db1bd3750a57617
Samuel-Jesus <samuel.jesan2018@gmail.com>
2021-10-26 17:11:16 -0300

    Fazendo requisição da API de dados Github, colocando no front

---
+++ main.js
@@ -0,0 +1,33 @@
+const LinksSocialMedia = { //Objeto que guarda as informações das redes socias (Endpoints da conta).
+    github: 'samuel-jesus',
+    youtube: 'youtube',
+    facebook: 'samuelJesus',
+    instagram: 'samweljesus',
+    twitter: 'SamJ57600550'
+  }
+  
+  function changeSocialMediaLinks() {
+    for (let li of socialLinks.children) {
+      const social = li.getAttribute('class')
+
+      console.log(li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`)
+    }
+  }
+
+  changeSocialMediaLinks()
+
+  function getGitHubProfilesInfos(){
+    const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        userName.textContent = data.name
+        userBio.textContent = data.bio
+        userLink.href = data.html_url
+        userPhoto.src = data.avatar_url
+        userLogin.textContent = data.login
+      })
+  }
+
+  getGitHubProfilesInfos()
